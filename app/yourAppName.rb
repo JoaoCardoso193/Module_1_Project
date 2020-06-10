94e1c17809c2649b15b6c4818a151f20a28c981c
robertalopez <roblop1234@yahoo.com>
2020-06-10 14:32:54 -0700

    finished all the main stuff!

--- app/yourAppName.rb
+++ app/yourAppName.rb
@@ -4,7 +4,7 @@ class App < TTY::Prompt
   # it is not an AR class so you need to add attr
 
   def run
-    welcome
+    # welcome
     login_or_signup
     main_menu
   end
@@ -13,7 +13,7 @@ class App < TTY::Prompt
 
   #method to get user input as an integer corresponding to an option
   #parameter 's' is the string printed to request user input, parameter 'limits' is the range of integers allowed as input
-  def int_input(s = "\nPlease select an option", limits = [1, 100])
+  def int_input(s = "\nPlease select an option:", limits = [1, 100])
     puts s
     user_input = gets.chomp
     #if user types 'exit', exit the application
@@ -25,8 +25,8 @@ class App < TTY::Prompt
     if user_input.to_i >= limits[0] and user_input.to_i <= limits[1]
         return user_input.to_i
     else
-        puts("Invalid input, please enter an integer value coresponding to an option, or type 'exit' to exit application.")
-        sleep(2)
+        puts("Invalid input, please enter an integer value coresponding to an option, or type 'exit' to exit application:".red.bold)
+        sleep(1)
         return int_input(s, limits)
     end
   end
@@ -43,19 +43,20 @@ class App < TTY::Prompt
 
 
 
-  #prints welcome message
-  def welcome
-    "Hello! Welcome to our app"
-    #sleep for a bit
-    sleep(0.3)
-    system 'clear'
-  end
+  # #prints welcome message
+  # def welcome
+  #   "Hello! Welcome to our app"
+  #   #sleep for a bit
+  #   sleep(0.3)
+  #   system 'clear'
+  # end
 
 
   #login method
   def login_or_signup
+    system 'clear'
     #login prompt, gets user input
-    puts "Please enter your username to sign up or log in"
+    puts "Please enter your username to sign up or log in:"
     name = gets.chomp.capitalize
     
     #checking for 'exit'
@@ -71,31 +72,33 @@ class App < TTY::Prompt
       # puts "Please enter password"
       # sleep(0.3)
       # password = gets.chomp
-     password = mask("Please Enter Your Password") 
+     password = mask("Please enter your password:".yellow.bold) 
       # if password == "exit"
       #   exit 
       # end
       if @student.password != password
-        puts "Incorrect password, please try again"
-        sleep(0.3)
+        system 'clear'
+        puts "Incorrect password, please try again:".red.bold 
+        sleep(0.5)
         login_or_signup
       end 
     else
-      print "Please enter your age"
+      print "Please enter your age:".yellow 
       age = int_input(s = "", limits = [1, 150])
       # puts "Please enter a password"
       # sleep(0.3)
       # password = gets.chomp 
-      password = mask("Please Enter Your Password") 
+      password = mask("Please enter your password:".yellow) 
       @student = Student.create(name: name, age: age, password: password)
     end
 
     #sleep briefly
-    sleep(1)
+    sleep(1.5)
 
     #Welcome student
-    puts "\nWelcome #{@student.name}!"
-    sleep(1)
+    system 'clear'
+    puts "\nWelcome #{@student.name}!".blue
+    sleep(1.5)
   end
 
 
@@ -105,7 +108,7 @@ class App < TTY::Prompt
 
     #presents options to student 
     enumerate_options(['Make an appointment', 'View upcoming appointments'])
-    user_input = int_input(s = "\nPlease select an option.", limits = [1, 2])
+    user_input = int_input(s = "\nPlease select an option:", limits = [1, 2])
     
     #Taking user to secondary menu depending on input
     if user_input == 1
@@ -121,43 +124,49 @@ class App < TTY::Prompt
     system 'clear'
 
     #Show all tutors
-    puts "Available tutors:"
+    puts "Available tutors:".magenta 
     tutors = Tutor.all.map { |tutor| "Name: #{tutor.name}, Subject: #{tutor.subject}, Years of experience: #{tutor.years_of_experience}"}
     enumerate_options(tutors)
 
     #requesting and storing user input
-    tutor = Tutor.find_by(id: int_input(s = "\nPlease select a tutor.", limits = [1, tutors.size]))
+    tutor = Tutor.find_by(id: int_input(s = "\nPlease select a tutor:", limits = [1, tutors.size]))
 
     #requesting appointment month
     months = {1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December"}
     enumerate_options(months.values)
-    month = int_input(s = "\nPlease select a month", limits = [1, 12])
+    month = int_input(s = "\nPlease select a month:", limits = [1, 12])
 
     #requesting appointment day
-    day = int_input(s = "\nPlease input a day", limits = [1, 31])
+    day = int_input(s = "\nPlease input a day:", limits = [1, 31])
 
     #requesting an hour
     good_appts = tutor.appointments.select{|appt| appt.taken == false && appt.begin_datetime.mday == day && appt.begin_datetime.mon == month}
-    display_hours = good_appts.map{|hora| "Start Time: #{hora.begin_datetime.hour}"}
+    if good_appts.size == 0 #considering the case when there are no appointments left
+      system 'clear'
+      puts 'No appointments available with this tutor on this day!'
+      sleep (1.5)
+      create_appointment_menu
+    end
+    display_hours = good_appts.map{|hora| "Start time: #{hora.begin_datetime.hour}"}
     enumerate_options(display_hours)
-    index = int_input(s = "\nPlease select an option", limits = [1, good_appts.size])
+    index = int_input(s = "\nPlease select an option:", limits = [1, good_appts.size])
     hour = good_appts[index - 1].begin_datetime.hour 
-    
 
     #requesting note
-    puts "\nPlease leave a note"
+    puts "\nPlease leave a note:"
     note = gets.chomp
+    system 'clear'
 
     #creating appointment
-    is_new = 
     appointment = @student.create_appointment(tutor, 2020, month, day, hour, note)
     if appointment != 'failed'
-      puts "\nAppointment created successfully on #{months[month]}, #{day} at #{hour} o'clock!"
+      puts "\nAppointment created successfully on #{months[month]}, #{day} at #{hour} o'clock!".green
+      sleep(1.5)
     end
 
     #returning home
     enumerate_options(['Create Another Appointment', 'Home'])
-    user_input = int_input(s = "\nPlease select an option.", limits = [1, 2])
+    user_input = int_input(s = "\nPlease select an option:", limits = [1, 2])
     if user_input == 1
       create_appointment_menu
     end
@@ -173,10 +182,10 @@ class App < TTY::Prompt
 
     #return to main menu if there are no upcoming appointments
     if appointments.size == 0
-      puts 'You have no upcoming appointments'
-      sleep(1)
+      puts 'You have no upcoming appointments.'
+      sleep(1.5)
       enumerate_options(['Make an appointment', 'Home'])
-      user_input = int_input(s = "\nPlease select an option.", limits = [1, 2])
+      user_input = int_input(s = "\nPlease select an option:", limits = [1, 2])
 
       if user_input == 1
         create_appointment_menu
@@ -194,25 +203,26 @@ class App < TTY::Prompt
 
     #present user options
     enumerate_options(['Cancel an appointment', 'Change appointment note', 'Home'])
-    user_input = int_input(s = "\nPlease select an option.", limits = [1, 3])
+    user_input = int_input(s = "\nPlease select an option:", limits = [1, 3])
 
     if user_input == 1
-      i = int_input(s = "\nPlease enter the number of the appointment you'd like to cancel.", limits = [1, appointments.size])
+      i = int_input(s = "\nPlease enter the number of the appointment you'd like to cancel:", limits = [1, appointments.size])
       @student.cancel_appointment(appointments[i-1].id)
-      puts "Appointment cancelled successfully!"
-      sleep(1)
+      puts "\nAppointment cancelled successfully!".green
+      sleep(1.5)
       view_upcoming_appointments_menu
     end
 
     if user_input == 2
-      i = int_input(s = "\nPlease enter the number of the appointment you'd like to change.", limits = [1, appointments.size])
+      i = int_input(s = "\nPlease enter the number of the appointment you'd like to change:", limits = [1, appointments.size])
       appointment = appointments[i-1]
-      puts "\nPlease enter the new appointment note"
+      system 'clear'
+      puts "\nPlease enter the new appointment note:"
       new_note = gets.chomp
       appointment.note = new_note
       appointment.save
-      puts "Appointment note changed successfully!"
-      sleep(1)
+      puts "\nAppointment note changed successfully!".green
+      sleep(1.5)
       view_upcoming_appointments_menu
     end
 
